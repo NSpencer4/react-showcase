dff0e4df18d4b00cc941be54cbeeb06f42b19173
Chase Spencer <cah-chase-spencer@cardinalhealth.com>
2020-10-18 18:04:17 -0400

    Responsive header init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
 
+function initializeApp(): void {
+  //
+}
+
 function App(): JSX.Element {
+  initializeApp();
   return <Header />;
 }
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import productPickerLogo from "../../logo.svg";
 import roundupLogo from "../../roundup-logo.png";
+import "./Header.scss";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Header extends React.Component {
   render(): JSX.Element {
     return (
-      <header className="header">
-        <img src={roundupLogo} className="roundup-logo" alt="roundup-logo" />
-        <img
-          src={productPickerLogo}
-          className="product-picker-logo"
-          alt="product-picker-logo"
-        />
+      <header className="row align-items-center">
+        <div className="col-xs-2 col-sm-3 col-md-3">
+          <img src={roundupLogo} id="roundup-logo" alt="roundup-logo" />
+        </div>
+        <div className="col-xs-9 col-sm-8 col-md-9">
+          <img
+            src={productPickerLogo}
+            id="product-picker-logo"
+            alt="product-picker-logo"
+          />
+        </div>
       </header>
     );
   }
