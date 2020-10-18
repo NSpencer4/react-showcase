c0bdf3ff5daf920d3735deba034cfe0c22222edb
Chase Spencer <cah-chase-spencer@cardinalhealth.com>
2020-10-18 14:13:24 -0400

    linter rules webpack starter config for code splitting and treeshaking (overkill but for learning purposes) JSDOM training to act like a real DOM .gitignore

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import "babel-polyfill";
+
+const jsdom: JSDOM = new JSDOM();
+
+// Setup browser environment so that we can test React components.
+global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
+global.window = document.parentWindow;
+global.navigator = {
+  userAgent:
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Mobile Safari/537.36\n",
+};
+global.console.debug = function () {}; // NodeJS does not have console.debug, but React uses it.
