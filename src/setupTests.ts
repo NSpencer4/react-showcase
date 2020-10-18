5f645b0f1deb6fe3f58f813eaeeb04904a1e1d69
Chase Spencer <cah-chase-spencer@cardinalhealth.com>
2020-10-18 14:41:50 -0400

    jsdom training for storage options sass support variables.scss defined for common theme

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,15 +3,39 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import "babel-polyfill";
+// import "babel-polyfill";
 
-const jsdom: JSDOM = new JSDOM();
+// Mock Classes
+class StorageMock {
+  store: any;
 
-// Setup browser environment so that we can test React components.
-global.document = jsdom.jsdom("<!doctype html><html><body></body></html>");
-global.window = document.parentWindow;
-global.navigator = {
-  userAgent:
-    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Mobile Safari/537.36\n",
-};
+  constructor() {
+    this.store = {};
+  }
+
+  clear(): void {
+    this.store = {};
+  }
+
+  getItem(key: string): string | null {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: string): void {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key: string): void {
+    delete this.store[key];
+  }
+}
+
 global.console.debug = function () {}; // NodeJS does not have console.debug, but React uses it.
+
+Object.assign(global.window, "localStorage", new StorageMock());
+Object.assign(global.window, "sessionStorage", new StorageMock());
+Object.assign(
+  global.navigator,
+  "userAgent",
+  "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Mobile Safari/537.36"
+);
